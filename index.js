db64dd43318e1747b8440dcce5b969f7c34f939f
Guillermon Luis <lguillermon1976@gmail.com>
2025-06-26 10:05:44 -0300

    test minimo funcional

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/webhook', (req, res) => {
-  console.log('📩 Mensaje recibido:', req.body);
-  res.send('✅ Mensaje recibido');
+app.post("/webhook", (req, res) => {
+  console.log("📨 Mensaje recibido:", req.body);
+  res.send('<Response><Message>Recibido OK ✅</Message></Response>');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor escuchando en puerto ${PORT}`);
 });
